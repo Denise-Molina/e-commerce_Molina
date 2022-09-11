412dcd711a956344d1c3d7e7ae07d78c61d8a388
Denise Molina <denimoli2000@gmail.com>
2022-09-11 20:36:52 -0300

    menú e-commerce

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <h1>
+        <strong>Sasha Pateles</strong>
+      </h1>
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
