6c7d995c42b9e124f72532b356ef846ad8fea2d4
Denise Molina <denimoli2000@gmail.com>
2022-10-02 23:59:43 -0300

    todavia no me funciona pero al menos no me tira errores

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Item from "./Item";
 //import ItemCount from  "./ItemCount";
-import getItems  from "../data";
-//import { useParams } from "react-router-dom";
-//import {getItemsByCategory} from "../data";
+import getItems from "../data";
+import { useParams } from "react-router-dom";
+import { getItemsByCategory } from "../data";
 
 function ItemListContainer() {
   let [data, setData] = useState([]);
-  //const {cat} = useParams();
-
+  const { cat } = useParams();
+  /*
   useEffect(() => {
       getItems().then((respuestaDatos) => setData(respuestaDatos));
   }, []);
+*/
+//codigo de prueba
+  useEffect(() => {
+    if (cat === undefined) {
+      getItems().then((respuestaDatos) => setData(respuestaDatos));
+    } else {
+      getItemsByCategory(cat).then((respuestaDatos) => setData(respuestaDatos));
+    }
+  }, [cat]);
 
   return (
     <div>
